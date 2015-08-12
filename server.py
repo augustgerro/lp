6e8d0b9852e5845788437f50ac90745e2fd1df7d
Aliaksei Braichuk <greensageam@gmail.com>
2015-08-12 09:40:28 +0300

    1

--- server.py
+++ server.py
@@ -12,11 +12,14 @@ import socket
 from tornado.options import define, options
 from pyquery import PyQuery
 
+CONVERSIONS=""
+import tornado.database
 define("port", default=8080, help="run on the given port", type=int)
 define("mysql_host", default="127.0.0.1:3306", help="mamba database host")
 define("mysql_database", default="lp", help="mamba database name")
 define("mysql_user", default="root", help="mamba database user")
 define("mysql_password", default="", help="mamba database password")
+#define("CONVERSIONS", default="", help="hz")
 
 
 class Application(tornado.web.Application):
@@ -37,7 +40,7 @@ class Application(tornado.web.Application):
         settings = dict(
             template_path=os.path.join(os.path.dirname(__file__), "templates"),
             static_path=os.path.join(os.path.dirname(__file__), "static"),
-            debug=False
+            debug=True
         )
 
         tornado.web.Application.__init__(self, handlers, **settings)
